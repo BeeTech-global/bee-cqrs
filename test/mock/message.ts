646c3b274d363d636ebba4775fd1a53f6a02f429
Vinicius Moraes <vinicius@vsmoraes.com>
2020-02-23 20:30:02 -0300

    Enable more strict code check

---
+++ test/mock/message.ts
@@ -0,0 +1,13 @@
+import { HandlerFunc, MessageBus } from '../../src/message/bus';
+
+export class Message {
+    public constructor(
+        public count: number = 0
+    ) {}
+}
+
+export class StubMessageBus extends MessageBus {
+    public execute<TMsg>(message: TMsg, handler: HandlerFunc): void {
+        return this.handle(message, handler);
+    }
+}
---
+++ test/mock/middleware.ts
@@ -0,0 +1,21 @@
+import { HandlerFunc } from '../../src/message/bus';
+
+export class MiddlewareFoo {
+    private foo: number = 0;
+
+    public constructor(init: number = 0) {
+        this.foo = init;
+    }
+
+    public getFooCound(): number {
+        return this.foo;
+    }
+
+    public handle(next: HandlerFunc): HandlerFunc {
+        return <TMsg, TResult>(msg: TMsg): TResult => {
+            this.foo++;
+            next(msg);
+            return null;
+        }
+    }
+}
